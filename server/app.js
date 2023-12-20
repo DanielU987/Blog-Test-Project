333b792c12bcd559e6c216012d2b420eb11542b6
Daniel <danikusov@yandex.ru>
2023-12-20 12:10:41 +0200

    commit

--- server/app.js
+++ server/app.js
@@ -6,64 +6,13 @@ const port = 3000;
 // Подключение маршрутов для постов
 app.use('/posts', postsRouter);
 
-// Отправка файла Main.vue при обращении к корневому маршруту
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'Main.vue'));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+// Обработка всех маршрутов Vue через index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}/`);
+  console.log(`Сервер запущен на порту http://localhost:${port}/`);
 });
-
-// Пример базы данных в памяти (для демонстрации)
-let posts = [
-  { id: 1, title: 'Заголовок поста 1', content: 'Содержание поста 1' },
-  { id: 2, title: 'Заголовок поста 2', content: 'Содержание поста 2' },
-  // Другие посты...
-];
-
-// Получение списка всех постов
-postsRouter.get('/', (req, res) => {
-  res.json(posts);
-});
-
-// Получение информации о конкретном посте
-postsRouter.get('/:id', (req, res) => {
-  const postId = parseInt(req.params.id);
-  const post = posts.find(post => post.id === postId);
-
-  if (post) {
-    res.json(post);
-  } else {
-    res.status(404).json({ error: 'Пост не найден' });
-  }
-});
-
-// Создание нового поста
-postsRouter.post('/', (req, res) => {
-  const newPost = req.body;
-  newPost.id = posts.length + 1;
-  posts.push(newPost);
-
-  res.status(201).json(newPost);
-});
-
-// Обновление информации о посте
-postsRouter.put('/:id', (req, res) => {
-  const postId = parseInt(req.params.id);
-  const updatedPost = req.body;
-
-  posts = posts.map(post => (post.id === postId ? updatedPost : post));
-
-  res.json(updatedPost);
-});
-
-// Удаление поста
-postsRouter.delete('/:id', (req, res) => {
-  const postId = parseInt(req.params.id);
-  posts = posts.filter(post => post.id !== postId);
-
-  res.json({ message: 'Пост успешно удален' });
-});
-
-module.exports = postsRouter;
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,19 +1,54 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-// Маршрут для создания нового поста
-router.post('/', (req, res) => {
-  // Логика для создания нового поста
+let posts = [
+  { id: 1, title: "Заголовок поста 1", content: "Содержание поста 1" },
+  { id: 2, title: "Заголовок поста 2", content: "Содержание поста 2" },
+  // Другие посты...
+];
+
+// Получение списка всех постов
+router.get("/", (req, res) => {
+  res.json(posts);
 });
 
-// Маршрут для изменения существующего поста
-router.put('/:id', (req, res) => {
-  // Логика для изменения поста с указанным идентификатором
+// Получение информации о конкретном посте
+router.get("/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === postId);
+
+  if (post) {
+    res.json(post);
+  } else {
+    res.status(404).json({ error: "Пост не найден" });
+  }
 });
 
-// Маршрут для удаления существующего поста
-router.delete('/:id', (req, res) => {
-  // Логика для удаления поста с указанным идентификатором
+// Создание нового поста
+router.post("/", (req, res) => {
+  const newPost = req.body;
+  newPost.id = posts.length + 1;
+  posts.push(newPost);
+
+  res.status(201).json(newPost);
+});
+
+// Обновление информации о посте
+router.put("/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const updatedPost = req.body;
+
+  posts = posts.map((post) => (post.id === postId ? updatedPost : post));
+
+  res.json(updatedPost);
+});
+
+// Удаление поста
+router.delete("/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  posts = posts.filter((post) => post.id !== postId);
+
+  res.json({ message: "Пост успешно удален" });
 });
 
 module.exports = router;
